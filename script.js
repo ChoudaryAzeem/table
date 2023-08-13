381aabb4d4b60cb1f97d65a4a817926c53444c35
ChoudaryAzeem <a5555azeem@gmail.com>
2023-08-13 21:08:15 +0500

    table commit

---
+++ script.js
@@ -0,0 +1,73 @@
+// let table =parseInt(prompt("Enter the Value "))
+// for(let i = 1; i <= 10; i++){
+//      document.write(`${table} x ${i}= ${table*i}` + "<br>")
+//  }
+
+ let x =  parseInt(prompt("Enter the Value"))
+ for(let u = 1; u <= 10; u++){+ "<br>"
+ document.write(`<h3>table of ${u}</h3> <br>` )
+          for(let i = 1; i <= 10; i++)
+          document.write(`${u} x ${i}= ${u*i}` + "<br>")
+      }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
